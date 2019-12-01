3d54cf64ec7a5de95c4b3b4a80010b6f9b1f622f
nath54 <cerisara.nathan@gmail.com>
2019-12-01 14:34:23 +0100

    a

--- script.py
+++ script.py
@@ -1,7 +1,13 @@
 
 
 def fonction():
-    #faire un pull git
+    # faire un pull du git repo pour avoir la derniere version
+    
+    # récupérer la dernière ligne du fichier de log
+    # l'analyser pour extraire le message
+    # modifier les fichiers du repo en consequence
+    # faire un commit et un push
+    
     
     
 
