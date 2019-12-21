461d68087712ace3cde8b566979fc2c024eae102
nath54 <cerisara.nathan@gmail.com>
2019-12-21 17:47:30 +0100

    a

--- makehtml.py
+++ makehtml.py
@@ -5,25 +5,21 @@ import io
 def make_page(ii):
     #0=nom categorie 1=l'adversaire 2=le lieu 3=l'heure 4=la date #5=joueurs 6=la journée
     print('TOO',ii)
-    txt="""
+    if len(ii)==8:
+        txt="""
 <html>
     <head>
         <meta charset="utf-8">
         <title>Houds Info """+ii[0]+"""</title>
         <link rel="stylesheet" href="https://olki.loria.fr/platform/style.7524b05e.css">
         <link href="page1.css" rel="stylesheet">
+        <script src="https://resultats.ffbb.com/js/resultat.js?JSVersion=1.0" type="text/javascript"></script>
     </head>
     <body>
-<<<<<<< HEAD
         <p id="u1x" value='"""+ii[0]+"""'></p>
         <p id="journee" value='"""+str(ii[6])+"""'></p>
-=======
-        <p id="u1x" value='"""+ii[0]+"""'></p>
-        <p id="journee" value='"""+str(ii[6])+"""'></p>
->>>>>>> 1e1941ad9707839e1107f8f8e31f2a2a60ad749a
         
         <div class="body-wrap boxed-container" style="background-color:rgb(200,250,200);">
-            <main>
             <section class="hero">
                 <div class="container">
                     <div class="hero-inner">
@@ -34,19 +30,12 @@ def make_page(ii):
                             <ul style='text-align: left; margin-top: 20'>
                                 <li>"""+ii[4]+""" ("""+ii[3]+""")</li>
                                 <li><strong>Lieu: </strong>"""+ii[2]+"""</li>
+                                <li><strong>Plan: </strong><a href='https://resultats.ffbb.com/here/here_popup.php?id="""+ii[7]+"""'>Lien</a></li>
                                 <li><strong>Adv: </strong>"""+str(ii[1])+"""</li>
                             </ul>
-                            <h2>Les joueurs :</h2>
                         </div>
                     </div>
                 </div>
-                <ul style='text-align: left; list-style-type: none;'>
-        """
-    for j in ii[5].keys():
-        txt+="""
-                <div class="joueur" id='"""+j+"""' style='color:black; border-radius:25px; padding-left:20px; padding-right:auto; background-color:rgb(40,120,60); border:2px solid black;'><li> """+j+""" : <span style='display: inline-block;'><input type="checkbox" id='"""+j+"""pr' name="present" onChange="onChangeBox('"""+j+"""pr')"><label for="present">Present</label></input></span> <span style='display: inline-block;'><input type="checkbox" id='"""+j+"""ab' name="absent" onChange="onChangeBox('"""+j+"""ab')><label for="absent">Absent</label></input></span> </li></div>"""
-    txt+="""
-                </ul>
             </section>
         </div>
         <script src="https://olki.loria.fr/n54hoods.js" type="text/javascript"></script>
@@ -54,10 +43,30 @@ def make_page(ii):
     </body>
 </html>
 """
-    # TODO: lancer un JS dans la page qui appele la fct "getUserState(u1X,journee,qui)" qui retourne "pr" ou "ab" ou "??" et qui met a jour les checkbox
-    # d'abord charger avec script src=... cette fonction dans https://olki.loria.fr/n54hoods.js
+        # TODO: lancer un JS dans la page qui appele la fct "getUserState(u1X,journee,qui)" qui retourne "pr" ou "ab" ou "??" et qui met a jour les checkbox
+        # d'abord charger avec script src=... cette fonction dans https://olki.loria.fr/n54hoods.js
 
-    f=open(ii[0]+".html","w")
-    f.write(txt)
-    f.close()
+        f=open(ii[0]+".html","w")
+        f.write(txt)
+        f.close()
+    else:
+        print(ii)
+        txt="""
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Houds Info """+ii[0]+"""</title>
+        <link rel="stylesheet" href="https://olki.loria.fr/platform/style.7524b05e.css">
+        <link href="page1.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Error : </h1>
+        <p>Pas de match trouvé</p>
+        <p>Désolé :(</p>
+    </body>
+</html>
+        """
+        f=open(ii[0]+".html","w")
+        f.write(txt)
+        f.close()
 
--- scrap.py
+++ scrap.py
@@ -18,18 +18,18 @@ cac="\n"
 cacc="|"
 ccac="@"
 
-txs="1"+cac
-txs+="1"+cac
-txs+="1"
+txs="1"+cac+"1"+cac+"1"
 
-if not nfs in os.listdir("./") or True:
+if not nfs in os.listdir("./"):
     f=open(nfs,'w')
     f.write( txs )
     f.close()
 
 f=open(nfs,'r')
-dataload=f.read().split(cac)
+dataload=f.read().strip().split(cac)
 f.close()
+
+print("aaaa",dataload)
 
 # 0=u17 1=u13 2=u9
 cats=["u17","u13","u9"]
@@ -63,12 +63,13 @@ joueurs=[ {
 
 rs=[200000002774034,200000002775683,200000002775749]
 ds=[200000002848356,200000002851801,200000002851984]
-ps=[int(data.split(cacc)[0]) for data in dataload]
+ps=[int(data.strip().split(cacc)[0]) for data in dataload if data!=""]
 print(ps)
 
 cat=0
 
 def scrapage():
+    print(cat,ps[cat])
     retel=True
     if "page"+str(cat)+".html" in os.listdir():
         retel=True
@@ -91,11 +92,16 @@ def scrapage():
     nbtd=0
     et=itr
     itd=itr
-    while itd!=-1:
+    sec=0
+    while itd!=-1 and sec<100:
         itd=txt.find("<td",et,ihoud)
         if itd!=-1:
             nbtd+=1
         et=itd+1
+        sec+=1
+    print(nbtd)
+    print(url)
+    if sec>=100: return [cats[cat],"error2"]
     if nbtd==5:
         j=txt.rfind("<td",0,ihoud)
         jj=txt.rfind("<td",0,j)
@@ -129,8 +135,18 @@ def scrapage():
         adv=txt[hh+1:hhh]
         lieu="Chez nous"
     else:
-        return "error"
-
+        return [cats[cat],"error"]
+    
+    plan=""
+    ip=txt.find("openhere('",ihoud)
+    bd=-2
+    if ip!=-1:
+        bd=txt.find("')",ip)
+        if bd!=-1 and ip+10<bd:
+            plan=txt[ip+10:bd]
+    
+    print("plan",plan,ip,bd)
+    
     date=txt[pcod+1:pcfd]
     heure=txt[pcoh+1:pcfh]
 
@@ -140,30 +156,37 @@ def scrapage():
     dd=d1.split("/")
     bd=date.split("/")
     
-    if int(dd[2])>int(bd[2]):
+    jour_sur_ordi=int(dd[2]+dd[1]+dd[0])
+    jour_sur_site=int(bd[2]+bd[1]+bd[0])
+    
+    if jour_sur_ordi>jour_sur_site:
         ps[cat]+=1
-        return "datetoup"
-    elif int(dd[1])>int(bd[1]):
-        ps[cat]+=1
-        return "datetoup"
-    elif int(dd[0])>int(bd[0]):
-        ps[cat]+=1
-        return "datetoup"
+        return [cats[cat],"datetoup"]
     
     
     #return "Pour la catégorie "+cats[cat]+", le prochain match est a "+heure+" le "+date+" contre "+adv
-    result=[cats[cat],adv,lieu,heure,date,joueurs[cat],ps[cat]]
+    result=[cats[cat],adv,lieu,heure,date,joueurs[cat],ps[cat],plan]
     return result #0=nom categorie 1=l'adversaire 2=le lieu 3=l'heure 4=la date #5=les joueurs de l'équipe 6=le plan
     
 
 
 for x in range(3):
     result=scrapage()
-    while result=="datetoup":
+    sec=0
+    while result[1]=="datetoup" and sec<50:
         result=scrapage()
+        sec+=1
     make_page(result)
     cat+=1
     print(result)
 
+txs=""
+for p in ps:
+    txs+=str(p)+cac
+txs=txs[:-1]
+
+f=open(nfs,"w")
+f.write(txs)
+f.close()
 
 
