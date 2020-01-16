39a56bf778b7dad0922d271927f4b0ec25f09a21
nath54 <cerisara.nathan@gmail.com>
2020-01-16 06:31:18 +0100

    aa

--- scrap.py
+++ scrap.py
@@ -42,7 +42,7 @@ f.close()
 # 0=u17 1=u13 2=u9
 cats=["u17","u13","u9"]
 
-"""
+
 joueurs=[ {
     "Nathan CERISARA":0,
     "Louis COLNAT":0,
@@ -70,10 +70,10 @@ joueurs=[ {
     "":0,
     }
 ]
-"""
 
-rs=[200000002774034,200000002775683,200000002775749]
-ds=[200000002848356,200000002851801,200000002851984]
+#https://resultats.ffbb.com/championnat/b5e6211ed412.html?r=200000002779163&d=200000002859412&p=1
+rs=[200000002779163,200000002775683,200000002775749]
+ds=[200000002859412,200000002851801,200000002851984]
 ps=[int(data.strip().split(cacc)[0]) for data in dataload if data!=""]
 maxdays=[int(data.strip().split(cacc)[0]) for data in dataload if data!=""]
 pg=["","",""]
@@ -92,11 +92,17 @@ def verif_days():
 
 def get_info_pages(cat,day):
     #ON OUVRE LA PAGE
-    f=io.open(dir_pages+get_name_page(cat,day),"r",encoding="utf-8")
-    txt=f.read()
-    f.close()
-    
     infos={"titre":None,"etat":"none","categorie":cat,"day":day,"adversaire":"adversaire inconnu","lieu":"lieu inconnu","date":"date inconnue","heure":"heure inconnue","plan":"plan inconnu"}
+    
+    name=get_name_page(cat,day)
+    if name in os.listdir(dir_pages):
+        f=io.open(dir_pages+name,"r",encoding="utf-8")
+        txt=f.read().lower()
+        f.close()
+    else:
+        infos["etat"]="pas match"
+        return infos
+    
     
     #ON CHERCHE LE TITRE :
     it1=txt.find("<title>")
@@ -105,7 +111,7 @@ def get_info_pages(cat,day):
         
         if it2>=it1: infos["titre"]=txt[it1+7:it2]
             
-    print("titre :",infos["titre"])
+    #print("titre :",infos["titre"])
     
     #SI LA PAGE A ETE TROUVEE :
     if infos["titre"]!=None or infos["titre"]!="404 Not Found":
@@ -115,14 +121,14 @@ def get_info_pages(cat,day):
         et=itr
         itd=itr
         sec=0 #securite
-        print(txt[et:ihoud])
+        #print(txt[et:ihoud])
         while itd!=-1 and sec<100:
             itd=txt.find("<td",et,ihoud)
             if itd!=-1:
                 nbtd+=1
             et=itd+1
             sec+=1
-        print("nbtd",nbtd)
+        #print("nbtd",nbtd)
         if sec>=100:
             infos["etat"]="error2"
             return infos
@@ -167,7 +173,7 @@ def get_info_pages(cat,day):
             bd=txt.find("')",ip)
             if bd!=-1 and ip+10<bd:
                 plan=txt[ip+10:bd]
-        print("plan",plan,ip,bd)
+        #print("plan",plan,ip,bd)
         date=txt[pcod+1:pcfd]
         heure=txt[pcoh+1:pcfh]
         dd=d1.split("/")
@@ -182,9 +188,9 @@ def get_info_pages(cat,day):
         infos["plan"]=plan
         infos["etat"]="good"
         
-        print("debugdate",jour_sur_ordi,jour_sur_site,cat,ps[cat]) 
+        #print("debugdate",jour_sur_ordi,jour_sur_site,cat,day) 
         if jour_sur_ordi>jour_sur_site:
-            ps[cat]+=1
+            ps[cats.index(cat)]+=1
             infos["etat"]="datetoup"
             return infos
     else:
@@ -199,18 +205,17 @@ def scrap_page(cat,day):
     f=io.open(dir_pages+get_name_page(cat,day),"w",encoding="utf-8")
     f.write(txt)
     f.close()
+    print("page scrapée ",cat,day)
 
 def an_page(cat):
+    day=ps[cats.index(cat)]
     print("Catégorie : ",cat," journée : ",ps[cats.index(cat)])
-    retel=True
+    retel=False
     if get_name_page(cat,ps[cats.index(cat)]) in os.listdir(dir_pages):
         retel=False
     if retel:
         scrap_page(cat,ps[cats.index(cat)])
     
-    f=io.open(dir_pages+get_name_page(cat,ps[cats.index(cat)]),"r",encoding="utf-8")
-    txt=f.read().lower()
-    f.close()
     
     infos_page=get_info_pages(cat,day)
     categorie=infos_page["categorie"]
@@ -219,14 +224,15 @@ def an_page(cat):
     heure=infos_page["heure"]
     date=infos_page["date"]
     plan=infos_page["plan"]
-    jrs=joueurs[cat]
-    day=ps[cat]
+    jrs=joueurs[cats.index(cat)]
+    day=ps[cats.index(cat)]
     
     etat=infos_page["etat"]
     if etat=="error": return [categorie,"error"]
     elif etat=="error2": return [categorie,"error2"]
     elif etat=="datetoup": return [categorie,"datetoup"]
     elif etat=="not found": return [categorie,"not found"]
+    elif etat=="pas match": return [categorie,"pas match"]
     
     #return "Pour la catégorie "+cats[cat]+", le prochain match est a "+heure+" le "+date+" contre "+adv
     #result=[cats[cat],adv,lieu,heure,date,joueurs[cat],ps[cat],plan]
@@ -237,36 +243,77 @@ def scrap_all_pages_of_cat(cat):
     sec=0
     etat=""
     day=1
-    print("\ncat : ",cat)
-    while etat!="404 Not Found" and sec<100:
+    #print("\ncat : ",cat)
+    while etat!="404 not found" and sec<50:
         scrap_page(cat,day)
         infos=get_info_pages(cat,day)
         etat=infos["titre"]
         day+=1
-        print("infos : ",infos," day : ",day)
+        sec+=1
+        print("aaaa",etat,sec)
+        #print("infos : ",infos," day : ",day)
     day-=1
-    print("cat : ",cat," day max : ",day," sec : ",sec)
-        
+    #print("cat : ",cat," day max : ",day," sec : ",sec)
+
+def todt(dt):
+	d=dt.split("/")
+	return int(d[2]+d[1]+d[0])
 
 def scrap_all():
     for cat in cats:
         scrap_all_pages_of_cat(cat)
 
+def get_all_dates_cat(cat):
+    ad=todt(d1)
+    dts={}
+    fs=os.listdir(dir_pages)
+    for f in fs:
+        print(f)
+        c=f.split("-")[1]
+        d=int(f.split("-")[2].split(".")[0])
+        if c==cat:
+            infos=get_info_pages(c,d)
+            dt=infos["date"]
+            if infos["etat"]=="good":
+                dts[d]=todt(dt)
+    dts=sorted(dts.items(), key=lambda t:t[1])
+    print( dts )
+    pm=0
+    for d in dts:
+        if ad<d[1]:
+            pm=d[0]
+            break
+    ps[cats.index(cat)]=pm
+	
+def verif_match(cat):
+    day=ps[cats.index(cat)]
+    infospg1=get_info_pages(cat,day)
+    scrap_page(cat,day)
+    infospg2=get_info_pages(cat,day)
+    if infospg1!=infospg2:
+        print(infospg1,"bbbbbbbb",infospg2)
+        #les dates sont différentes
+        scrap_all_pages_of_cat(cat)
+        get_all_dates_cat(cat)
+
 ########################################### - MAIN CODE - ###########################################
 
-
+#TODO : a debloquer une fois que j'aurais tout fait sur le server
 """
-for cat of cats:
-    result=an_page(cat)
-    sec=0
-    while result[1]=="datetoup" and sec<50:
-        result=an_page(cat)
-        sec+=1
-    pg[x]=make_page(result)
-    print(result)
+#scrap_all()
+
+for cat in cats:
+    get_all_dates_cat(cat)
+    verif_match(cat)
+
+pg=[]
+for cat in cats:
+	pg.append( make_page( an_page(cat) ) )
 
 make_final_page(pg[2],pg[1],pg[0])
+"""
 
+"""
 txs=""
 for p in ps:
     txs+=str(p)+cac
